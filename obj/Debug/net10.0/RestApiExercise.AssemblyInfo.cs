d2aafa17aedd326cffa221b8fe1606abc7f3b6d7
HarshiniSupriyaJ <conhsup@partner.waters.com>
2026-07-30 10:49:14 +0530

    Address review feedback

--- obj/Debug/net10.0/RestApiExercise.AssemblyInfo.cs
+++ obj/Debug/net10.0/RestApiExercise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestApiExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03acc3c9aa2bee7bcc8419771754127902051006")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestApiExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestApiExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
